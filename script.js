6ac5a01d6478a408c03bd90aef83cb6f0f9ef3ab
synxther <synxther@protonmail.com>
2022-12-28 17:25:07 -0300

    emoji themes transitions click

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function myFunction() {
     var element = document.body;
+    //add ou remover a classe dark
     element.classList.toggle("dark");
+    //verifica se existe a classe dark
+    if(element.classList.contains("dark")) {
+        document.getElementById("theme").innerHTML="🌞"
+    }
+    else {
+        document.getElementById("theme").innerHTML="🌚"}
  }
+
