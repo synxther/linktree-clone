9675b9edfcd59e63c956996096c5bbed133fda53
synxther <synxther@protonmail.com>
2023-01-11 12:57:49 -0300

    bug fixes and application design changes

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,8 @@
 function myFunction() {
     var element = document.body;
-    //add ou remover a classe dark
+    //add or remove class dark
     element.classList.toggle("dark");
-    //verifica se existe a classe dark
+    //check se existe a classe dark
     if(element.classList.contains("dark")) {
         document.getElementById("theme").innerHTML="🌞"
     }
